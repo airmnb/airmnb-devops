34894948e786b0346280cdedd971e8623b98671b
Jun Shao <mr.shaojun@gmail.com>
2018-04-24 21:47:46 +1000

    Fix eol in arimnb.conf

--- clear.sh
+++ clear.sh
@@ -3,4 +3,5 @@
 rm -rf /var/www/airmnb/assets/*
 rm -f /var/www/airmnb/current
 rm -f /var/www/airmnb/certs
+rm -f /etc/apache2/sites-enabled/airmnb.conf
 rm -f /etc/apache2/sites-available/airmnb.conf
