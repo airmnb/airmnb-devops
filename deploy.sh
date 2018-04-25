0e489749f9517ec05e58c196a4538cfb769eebb4
Jun Shao <mr.shaojun@gmail.com>
2018-04-25 13:12:43 +1000

    Fix file name typo

--- deploy.sh
+++ deploy.sh
@@ -84,7 +84,7 @@ npm run build
 # Marry app and web
 echo -e "${GREEN}Marry app and web${NC}"
 ln -s $assetdir/web/build/index.html $assetdir/app/app/index.html
-ln -s $assetdir/web/build/service-worker.js $assetdir/app/app/service-workder.js
+ln -s $assetdir/web/build/service-worker.js $assetdir/app/app/service-worker.js
 ln -s $assetdir/web/build/static $assetdir/app/app/static
 
 # Switch symlink
